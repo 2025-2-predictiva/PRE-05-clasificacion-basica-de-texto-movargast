30d6d6ce91a8926feb42c31d1b4a8fac22fc3dc0
movargast <movargast@unal.edu.co>
2025-09-20 12:24:21 -0500

    Entrega Taller 5

--- tests/test_homework.py
+++ tests/test_homework.py
@@ -15,10 +15,10 @@ def test_01():
         compression="zip",
     )
 
-    with open("homework/clf.pickle", "rb") as file:
+    with open("homework/clf.pkl", "rb") as file:
         clf = pickle.load(file)
 
-    with open("homework/vectorizer.pickle", "rb") as file:
+    with open("homework/vectorizer.pkl", "rb") as file:
         vectorizer = pickle.load(file)
 
     accuracy = accuracy_score(
